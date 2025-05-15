8b4dc329466df3d1dd4ec1fd28086933656fac76
Daniil Matveev <d.matveev@voiso.com>
2025-05-15 16:56:37 +0400

    fix: add outputs for new nodes

--- packages/examples/src/helpers.tsx
+++ packages/examples/src/helpers.tsx
@@ -1,8 +1,8 @@
 import { Node } from "@voiso/react-flow-editor"
 import { action } from "nanostores"
 
-import { NodesAtom } from "./store"
-import { DEFAULT_INPUT } from "./constants"
+import { NodesAtom, OutputsAtom } from "./store"
+import { DEFAULT_INPUT, DEFAULT_OUTPUT, DEFAULT_OUTPUT_2 } from "./constants"
 
 export const nodeFactory = (nodeName?: string): Node => ({
   id: nodeName || `Node_${(Math.random() * 10000).toFixed()}`,
@@ -12,4 +12,35 @@ export const nodeFactory = (nodeName?: string): Node => ({
   state: null
 })
 
-export const createNode = action(NodesAtom, "createNode", (store) => store.set(store.get().concat([nodeFactory()])))
+export const outputFactory = (nodeId: string) => {
+  const number = (Math.random() * 10000).toFixed()
+  const id = `Output_${number}`
+  const id_2 = `Output_${number + 1}`
+
+  return [
+    {
+      id,
+      nodeId,
+      nextNodeId: null,
+      position: DEFAULT_OUTPUT
+    },
+    {
+      id: id_2,
+      nodeId,
+      nextNodeId: null,
+      position: DEFAULT_OUTPUT_2
+    }
+  ]
+}
+
+export const createNode = action(NodesAtom, "createNode", (store) => {
+  const newNode = nodeFactory()
+
+  store.set(store.get().concat([newNode]))
+
+  const newOutputs = outputFactory(newNode.id)
+
+  OutputsAtom.set(OutputsAtom.get().concat([...newOutputs]))
+
+  return newNode
+})
